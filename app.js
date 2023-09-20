b45d234f9c8b85213de4eaca83e9d733273dbf3b
Nathan <nathansilva406@gmail.com>
2023-09-20 15:36:19 -0300

    Arquivo raiz pronto

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const rotaLivros = require("./routes/livros")
+const rotaFavoritos = require("./routes/favoritos")
+const cors = require("cors")
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json()) // para receber um corpo de req em json
+app.use(cors( {origin: "*"} ))
 
 app.use("/livros", rotaLivros)
+app.use("/favoritos", rotaFavoritos)
 
 app.listen(8000, () => {
     console.log("servidor rodando");
